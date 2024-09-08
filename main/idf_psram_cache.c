6ce3457bbebd187cee7d7815f4df11c79a729053
Alwin Arrasyid <alwin.ridd@gmail.com>
2024-09-08 21:00:39 +0700

    refactor to reduce duplication

--- main/idf_psram_cache.c
+++ main/idf_psram_cache.c
@@ -3,6 +3,7 @@
 #include <esp_timer.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
+#include <soc/dport_reg.h>
 #include <stdint.h>
 #include <stdio.h>
 
@@ -15,52 +16,59 @@ void fill_memory(uint8_t *addr, size_t length, uint8_t value) {
   }
 }
 
+float test_memory_access(uint8_t *ptr, size_t size, size_t n_iter,
+                         uint8_t value) {
+  int64_t start = esp_timer_get_time();
+  for (int i = 0; i < n_iter; i++) {
+    fill_memory(ptr, size, value);
+  }
+  int64_t end = esp_timer_get_time();
+  return (end - start) / (n_iter * 1.0f);
+}
+
 void app_main(void) {
   vTaskDelay(1000 / portTICK_PERIOD_MS);
+
+  /* Refer to the ESP32 Technical Reference Manual for the cache mode register.
+   * If the mode is 0, then each CPU core uses its own cache pool
+   */
+  // uint64_t cache_mode = DPORT_REG_READ(DPORT_CACHE_MUX_MODE_REG);
+  // printf("Cache mode: %llu\n", cache_mode);
 
   /* Re-configure the task watchdog timer to 30 seconds */
   esp_task_wdt_deinit();
   esp_task_wdt_init(30, false);
 
-  /* malloc with larger than 8 bytes will allocate memory on the PSRAM */
-	heap_caps_malloc_extmem_enable(128*1024);
+  /* malloc with larger than 128 KB will allocate memory on the PSRAM */
+  heap_caps_malloc_extmem_enable(128 * 1024);
 
   /* allocate 64KB of memory on the PSRAM to demonstrate cache hit (at 32KB max)
    * and cache miss */
   uint8_t *data = (uint8_t *)heap_caps_malloc(ALLOC_SIZE, MALLOC_CAP_SPIRAM);
 
-  int64_t start;
-  int64_t end;
   float average_time;
 
-  start = esp_timer_get_time();
-  for (int i = 0; i < ITER_NUM; i++) {
-    fill_memory(data, ALLOC_SIZE / 2, 255);
-  }
-  end = esp_timer_get_time();
-  average_time = (end - start) / (ITER_NUM * 1.0f);
-  printf("Average time: %f us, n_iter: %d, write_size: %d, memory address: %p\n", average_time,
-         ITER_NUM, ALLOC_SIZE / 2, data);
+  average_time = test_memory_access(data, ALLOC_SIZE / 2, ITER_NUM, 255);
+  printf(
+      "Average time: %f us, n_iter: %d, write_size: %d, memory address: %p\n",
+      average_time, ITER_NUM, ALLOC_SIZE / 2, data);
 
-  start = esp_timer_get_time();
-  for (int i = 0; i < ITER_NUM; i++) {
-    fill_memory(data, ALLOC_SIZE, 255);
-  }
-  end = esp_timer_get_time();
-  average_time = (end - start) / (ITER_NUM * 1.0f);
-  printf("Average time: %f us, n_iter: %d, write_size: %d, memory address: %p\n", average_time,
-				 ITER_NUM, ALLOC_SIZE, data);
+  average_time = test_memory_access(data, ALLOC_SIZE, ITER_NUM, 255);
+  printf(
+      "Average time: %f us, n_iter: %d, write_size: %d, memory address: %p\n",
+      average_time, ITER_NUM, ALLOC_SIZE, data);
 
-	uint8_t *internal_data = (uint8_t *)malloc(ALLOC_SIZE);
-	start = esp_timer_get_time();
-	for (int i = 0; i < ITER_NUM; i++) {
-		fill_memory(internal_data, ALLOC_SIZE / 2, 255);
-	}
-	end = esp_timer_get_time();
-	average_time = (end - start) / (ITER_NUM * 1.0f);
-	printf("Average time: %f us, n_iter: %d, write_size: %d, memory address: %p\n", average_time,
-				 ITER_NUM, ALLOC_SIZE / 2, internal_data);
+  uint8_t *internal_data = (uint8_t *)malloc(ALLOC_SIZE);
+  average_time =
+      test_memory_access(internal_data, ALLOC_SIZE / 2, ITER_NUM, 255);
+  printf(
+      "Average time: %f us, n_iter: %d, write_size: %d, memory address: %p\n",
+      average_time, ITER_NUM, ALLOC_SIZE / 2, internal_data);
 
+  average_time = test_memory_access(internal_data, ALLOC_SIZE, ITER_NUM, 255);
+  printf(
+      "Average time: %f us, n_iter: %d, write_size: %d, memory address: %p\n",
+      average_time, ITER_NUM, ALLOC_SIZE, internal_data);
   while (1) {
     vTaskDelay(1000 / portTICK_PERIOD_MS);
   }
